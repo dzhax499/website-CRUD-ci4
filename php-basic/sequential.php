62b63c4e4d80b1586a53c723070c3b7d5e604ee5
dzhax499 <dzdzakir@gmail.com>
2025-08-31 23:04:52 +0700

    php basic: sequential

---
+++ php-basic/sequential.php
@@ -0,0 +1,12 @@
+<!--  1 Program PHP : Instruksi Berurutan	 -->
+
+<?php 
+echo "biodata diri <br>"; // <br> agar terlihat di web nya break tetapi jika di terminal maka akan tercetak break
+$nama = "Dzakir Tsabit";
+$nim = "241511071";
+$prodi = "D3 Teknik Informatika";
+
+echo "Nama : $nama <br>";
+echo "Nim: $nim <br>";
+echo "Prodi: $prodi <br>";
+?>
