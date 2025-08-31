7393ae69c85c3379ad2752e11bf9fb6bfea2d2bc
dzhax499 <dzdzakir@gmail.com>
2025-08-31 23:30:45 +0700

    php-basic: selection

---
+++ php-basic/selection.php
@@ -0,0 +1,13 @@
+<!-- 2 Program PHP : Instruksi Berurutan	 -->
+
+<?php
+$poin = 100;
+
+if ($poin >= 80) {
+    echo "Selamat anda mendapatkan A !";
+} elseif ($poin >= 70) {
+    echo "Selamat anda mendapatkan B !";
+} else {
+    echo "Selamat anda mendapatkan C !";
+}
+?>
