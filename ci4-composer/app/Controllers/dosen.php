0780be97b53369e427a305250e7fcd6e2e74e21d
dzhax499 <dzdzakir@gmail.com>
2025-09-03 13:29:29 +0700

    ci4 composer learn 1

---
+++ ci4-composer/app/Controllers/dosen.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+class dosen extends BaseController
+{
+    public function display(): string
+    {
+        return ('ello ganti class');
+    }
+}
--- ci4-composer/app/Controllers/haii.php
+++ ci4-composer/app/Controllers/haii.php
@@ -2,10 +2,10 @@
 
 namespace App\Controllers;
 
-class Home extends BaseController
+class haii extends BaseController
 {
     public function index(): string
     {
-        return ('ello');
+        return view(name:'hello');
     }
 }
